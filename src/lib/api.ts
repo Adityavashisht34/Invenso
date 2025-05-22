211ea2ae8ff7db322212018acb04a52fdfce519b
Aditya Vashisht <112467903+Adityavashisht34@users.noreply.github.com>
2025-05-22 21:58:02 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://inventorymanagement-cf18.onrender.com/api';
+const API_URL = process.env.API_URL;
 
 const api = {
   setToken: (token: string) => {
